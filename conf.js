4519fd2c04c3aab26a9011a3d38880c1fdf2584b
Costa, Flavius <fcosta@teksystems.com>
2017-04-06 15:00:33 -0400

    Start tests with browser windows maximized and tell programatically on which browser test is executing

--- conf.js
+++ conf.js
@@ -9,6 +9,19 @@ exports.config = {
         'browserName': 'chrome',
         specs: [
             'tests/todo-spec2.js'
-        ]
+        ],
+        chromeOptions: {
+            args: [
+                '--start-maximized'
+            ]
+        }
     }],
+
+    onPrepare: function () {
+        browser.manage().timeouts().implicitlyWait(5000);
+        browser.driver.getCapabilities().then(function (caps) {
+            if (caps.get('browserName') === 'firefox')
+                browser.manage().window().maximize();
+        });
+    }
 };
